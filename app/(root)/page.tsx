b90a9217432d49a76db6f10a6c83532c414cf5a9
Victor Dutra <vsdutraa@gmail.com>
2024-11-04 04:32:02 -0300

    clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const Dashboard = () => {
+  return (
+    <div className="p-4">
+      <UserButton />
+    </div>
+  );
+};
+
+export default Dashboard;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from "next";
-
-import { Outfit } from "next/font/google";
-
 import "./globals.css";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Outfit } from "next/font/google";
 
 const font = Outfit({ subsets: ["latin"] });
 
@@ -17,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
