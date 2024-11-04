7d4b68c2024f7ee7fc0ca8d64350ffd21f2864da
Victor Dutra <vsdutraa@gmail.com>
2024-11-04 04:12:39 -0300

    environment setup

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+import { Outfit } from "next/font/google";
+
+import "./globals.css";
+
+const font = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
